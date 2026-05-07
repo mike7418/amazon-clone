f3aa15f40b27bcda53605914c00d53fbaf5b8c79
mike7418 <parksmike@dwx.com>
2026-05-07 15:06:36 -0500

    upload for FES to diag

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
+import { 
+  Router, 
+  Routes, 
+  Route, 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  Outlet, 
+  RouterProvider 
+} from 'react-router-dom'
 import Header from './components/header/Header'
-import Banner from './components/home/Banner'
 import Footer from './components/footer/Footer'
+import Home from './pages/Home'
+import { div } from 'framer-motion/client'
+
+const Layout = () => {
+  return (
+    <div>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
 
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Layout/>}/>
+      <Route path="/home" element={<Home/>}/>
+    </Route>
+  ))
  
   return (
     <div className='font-bodyFont h-screen w-full'>
-      <Header />
-      <Banner />
-      <Footer />
+     <RouterProvider router={router}> </RouterProvider>
     </div>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../pages/Home'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner/>
+    </div>
+  )
+}
+
+export default Home
