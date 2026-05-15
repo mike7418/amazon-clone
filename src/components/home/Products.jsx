d83e607ccd036422a42d91c2873e9c387afc0717
mike7418 <parksmike@dwx.com>
2026-05-15 14:55:08 -0500

    continueing on

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -10,21 +10,20 @@ const Products = () => {
     const productData = data;
 
   return (
-    <div className='auto max-w-screen-2xl grid grid-cols-4 mt-176 gap-4 grow-0'>
+    <div className='z-auto max-w-screen-2xl grid grid-cols-4 mt-176 gap-10 grow-0 mb-10 '>
         {
             productData.map((item) => (
-                <div className='bg-white p-6  w-64 z-10'>
+                <div className='bg-white p-6 w-64 z-10 hover:scale-105 hover:shadow-2xl rounded-lg transition-transform duration-300 rounded-lg shadow-md ' key={item.id}>
                 <div key={item.id}>
                     <img src={item.category.image} alt="ProductImg" className='w-52 h-52 object-contain' />
                 </div>
-                <div className='mt-4 w-52'>
+                <div className='mt-4 w-52 flex justify-between gap-2 font-semibold text-sm'>
                     <h2>{item.title}</h2>
                     <p>{`$${item.price.toFixed(2)}`}</p>
                 </div>
                 </div>
             ))
-        }
-        { console.log(productData) }
+        }        
     </div>
   )
 }
